c6ccb3dc9c085e3d4c3603e65ae4a5316a3f223b
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-05-31 05:00:38 +0530

    Update 9.sql

--- sql-practice/9.sql
+++ sql-practice/9.sql
@@ -1,3 +1,7 @@
 -- Show the first_name, last_name, and height of the patient with the greatest height.
-SELECT first_name, last_name, MAX(height)
-FROM patients;
+SELECT 
+    first_name, 
+    last_name, 
+    MAX(height)
+FROM 
+    patients;
