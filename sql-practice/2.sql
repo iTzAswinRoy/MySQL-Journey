0b7295d5e09e95a8c26866dd7d726cff773fc43e
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-05-31 04:57:53 +0530

    Update 2.sql

--- sql-practice/2.sql
+++ sql-practice/2.sql
@@ -1,4 +1,8 @@
 -- Show first name and last name of patients who does not have allergies. (null)
-SELECT first_name, last_name 
-FROM patients
-WHERE allergies Is NULL;
+SELECT 
+    first_name, 
+    last_name 
+FROM 
+    patients
+WHERE 
+    allergies Is NULL;
