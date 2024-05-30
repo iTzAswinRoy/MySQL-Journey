82bb53f1f5924aa6c3acf1fe9519513e6878b0b6
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-05-31 04:59:22 +0530

    Update 7.sql

--- sql-practice/7.sql
+++ sql-practice/7.sql
@@ -1,5 +1,11 @@
 -- Show first name, last name, and the full province name of each patient.
-SELECT first_name, last_name, province_name
-FROM patients AS p
-JOIN province_names as pn
-ON p.province_id = pn.province_id;
+SELECT 
+    first_name, 
+    last_name, 
+    province_name
+FROM 
+    patients AS p
+JOIN 
+    province_names as pn
+ON 
+    p.province_id = pn.province_id;
