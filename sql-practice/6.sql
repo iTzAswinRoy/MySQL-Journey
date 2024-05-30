838cfa91feca1025eb7ccc8a0dd495ed52055b19
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-05-31 04:58:59 +0530

    Update 6.sql

--- sql-practice/6.sql
+++ sql-practice/6.sql
@@ -1,4 +1,7 @@
 -- Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
-UPDATE patients
-SET allergies = 'NKA'
-WHERE allergies IS NULL;
+UPDATE 
+    patients
+SET 
+    allergies = 'NKA'
+WHERE 
+    allergies IS NULL;
