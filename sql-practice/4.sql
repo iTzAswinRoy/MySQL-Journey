9b03a4dc8bcdbdc655d52463213f1f42c84fae7a
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-05-31 04:58:28 +0530

    Update 4.sql

--- sql-practice/4.sql
+++ sql-practice/4.sql
@@ -1,4 +1,8 @@
 -- Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
-SELECT first_name, last_name
-FROM patients
-WHERE weight BETWEEN 100 AND 120;
+SELECT 
+    first_name, 
+    last_name
+FROM 
+    patients
+WHERE 
+    weight BETWEEN 100 AND 120;
