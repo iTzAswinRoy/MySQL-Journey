b78d92cea8a7e249d0fe3916cf3570f07fdf1e6d
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-04 00:00:01 +0530

    Create 20-1.sql

---
+++ sql-practice/20-1.sql
@@ -0,0 +1,9 @@
+-- Display every patient's first_name.
+-- Order the list by the length of each name and then by alphabetically.
+SELECT 
+		first_name
+FROM 
+		patients
+ORDER BY 
+		LENGTH(first_name) ASC, 
+		first_name;  
