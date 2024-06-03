dec0a0563add6b8f7b32995bce0aed8f5cda8af0
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-03 23:59:26 +0530

    Create 21.sql

---
+++ sql-practice/21.sql
@@ -0,0 +1,13 @@
+-- Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+SELECT 
+		a.patient_id, 
+		p.first_name, 
+		p.last_name
+FROM 
+		patients AS p
+JOIN 
+		admissions AS a
+ON 
+		p.patient_id = a.patient_id
+WHERE 
+		a.diagnosis = 'Dementia';  
