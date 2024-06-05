d1000e6a7cc7b1d19b0cc9943cf6236f7caee119
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-05 23:13:09 +0530

    Create 27.sql

---
+++ sql-practice/27.sql
@@ -0,0 +1,12 @@
+-- Show all allergies ordered by popularity. Remove NULL values from query.
+SELECT 
+		allergies, 
+		COUNT(*) AS popularity
+FROM 
+		patients
+WHERE 
+		COALESCE(allergies, '') <> ''
+GROUP BY 
+		allergies
+ORDER BY 
+		popularity DESC;
