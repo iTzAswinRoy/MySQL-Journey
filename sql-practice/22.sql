f77935d5f36f149c061462da4958e90981def66b
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-05 23:10:41 +0530

    Create 22.sql

---
+++ sql-practice/22.sql
@@ -0,0 +1,15 @@
+-- Show the total amount of male patients and the total amount of female patients in the patients table.
+-- Display the two results in the same row.
+SELECT 
+		(SELECT 
+					COUNT(*) 
+			FROM 
+					patients
+			WHERE 
+					gender = 'M') AS male_count, 
+					(SELECT 
+								count(*) 
+					 FROM 
+								patients
+			     WHERE 
+						    gender = 'F') AS female_count
