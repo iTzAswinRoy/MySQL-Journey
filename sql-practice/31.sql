53e02fd0877062ff87483eb8b2e47c63fa42efe4
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-07 14:40:55 +0530

    Create 31.sql

---
+++ sql-practice/31.sql
@@ -0,0 +1,7 @@
+-- Show the difference between the largest weight and smallest weight for patients with the last name 'Maroni'
+SELECT 
+		(MAX(weight) - MIN(weight)) AS difference
+FROM 
+		patients
+WHERE 
+		last_name = 'Maroni';
