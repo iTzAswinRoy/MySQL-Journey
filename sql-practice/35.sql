9578abe9b62aa9be7944a8d4aad9071b8d1bce25
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-08 18:54:15 +0530

    Create 35.sql

---
+++ sql-practice/35.sql
@@ -0,0 +1,14 @@
+-- Show first_name, last_name, and the total number of admissions attended for each doctor.
+-- Every admission has been attended by a doctor.
+SELECT 
+		d.first_name, 
+		d.last_name, 
+		COUNT(a.admission_date)
+FROM 
+		admissions AS a
+JOIN 
+		doctors AS d 
+ON 
+		a.attending_doctor_id = d.doctor_id
+GROUP BY 
+		attending_doctor_id;
