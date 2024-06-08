ddf3dec6596358630902b994dfa4af69cad0da59
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-08 18:56:22 +0530

    Create 37.sql

---
+++ sql-practice/37.sql
@@ -0,0 +1,14 @@
+-- Display the total amount of patients for each province. Order by descending.
+SELECT 
+		pn.province_name, 
+		COUNT(p.patient_id) As total
+FROM 
+		patients AS p 
+JOIN 
+		province_names AS pn 
+ON 
+		p.province_id = pn.province_id
+GROUP BY 
+		pn.province_id
+ORDER BY 
+		total DESC;
