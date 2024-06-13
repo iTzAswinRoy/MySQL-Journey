345c338db9bb0f3b7120cec92617f9e956575449
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-13 23:43:48 +0530

    Create 39.sql

---
+++ sql-practice/39.sql
@@ -0,0 +1,12 @@
+-- display the first name, last name and number of duplicate patients based on their first name and last name.
+-- Ex: A patient with an identical name can be considered a duplicate.
+SELECT 
+		first_name, 
+		last_name, 
+		COUNT(*) AS duplicates 
+FROM 
+		patients
+GROUP BY 
+		first_name, last_name
+HAVING 
+		duplicates > 1;
