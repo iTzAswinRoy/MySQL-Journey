8caa614549662961faadae40474722f0d0f4caf3
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-14 19:28:44 +0530

    Create 46.sql

---
+++ sql-practice/46.sql
@@ -0,0 +1,19 @@
+-- Each admission costs $50 for patients without insurance, and $10 for patients with insurance. 
+-- All patients with an even patient_id have insurance.
+-- Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. 
+-- Add up the admission_total cost for each has_insurance group.
+SELECT 
+		(CASE 
+				WHEN patient_id % 2 == 0 THEN 'Yes'
+        ELSE 
+		        'No'
+        END) AS insurance, 
+        SUM(CASE 
+			        WHEN patient_id % 2 == 0  THEN 10
+              ELSE 
+					        50
+              END) AS total
+FROM 
+		admissions
+GROUP BY 
+		insurance;
