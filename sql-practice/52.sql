1f7f2466841e92b62b9edc29d8779c72cc113cbc
iTzAswinRoy <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-14 19:31:25 +0530

    Create 52.sql

---
+++ sql-practice/52.sql
@@ -0,0 +1,16 @@
+-- We need a breakdown for the total amount of admissions each doctor has started each year. 
+-- Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.
+SELECT 
+    d.doctor_id, 
+    CONCAT(d.first_name, ' ', d.last_name) AS full_name, 
+    d.specialty, 
+    YEAR(a.admission_date) AS admission_year, 
+    COUNT(*) AS total_admissions
+FROM 
+    admissions AS a 
+LEFT JOIN 
+    doctors AS d   
+ON 
+    a.attending_doctor_id = d.doctor_id
+GROUP BY 
+    d.doctor_id, full_name, specialty, admission_year;
