e0f6ada64065609e5e45ffaeb4203ed5d60e6ffb
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-15 21:19:11 +0530

    Create 54.sql

---
+++ sql-practice/54.sql
@@ -0,0 +1,8 @@
+-- Show the category_name and description from the categories table sorted by category_name.
+SELECT 
+		category_name, 
+    description 
+FROM 
+		categories
+ORDER BY
+		category_name;
