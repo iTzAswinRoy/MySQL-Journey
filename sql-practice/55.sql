6ff43b7dfb7b79997393e16468eebb99faefb0dd
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-15 21:23:54 +0530

    Create 55.sql

---
+++ sql-practice/55.sql
@@ -0,0 +1,9 @@
+-- Show all the contact_name, address, city of all customers which are not from 'Germany', 'Mexico', 'Spain'
+SELECT 
+		contact_name,
+    address,
+    city
+FROM 
+		customers
+WHERE
+		country NOT IN ('Germany', 'Mexico', 'Spain');
