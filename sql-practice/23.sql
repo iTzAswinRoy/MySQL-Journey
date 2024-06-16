eab91363c170fcaf8d5b3ec0a324e117f2989b7b
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:08:24 +0530

    Update 23.sql

--- sql-practice/23.sql
+++ sql-practice/23.sql
@@ -1,13 +1,13 @@
 -- Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
 SELECT 
-		first_name, 
-		last_name, 
-		allergies
+    first_name, 
+    last_name, 
+    allergies
 FROM 
-		patients
+    patients
 WHERE 
-		allergies IN ('Penicillin', 'Morphine')
+    allergies IN ('Penicillin', 'Morphine')
 ORDER BY 
-		allergies ASC, 
-		first_name ASC, 
-		last_name ASC;
+    allergies ASC, 
+    first_name ASC, 
+    last_name ASC;
