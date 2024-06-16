da3a02badda39f6502186e943b7a8f5df4b4e65f
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:19:01 +0530

    Update 44.sql

--- sql-practice/44.sql
+++ sql-practice/44.sql
@@ -2,21 +2,21 @@
 -- Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
 -- Check patients, admissions, and doctors tables for required information.
 SELECT 
-		p.patient_id, 
-		p.first_name, 
-		p.last_name, 
-		d.specialty
+    p.patient_id, 
+    p.first_name, 
+    p.last_name, 
+    d.specialty
 FROM 
-		patients AS p 
+    patients AS p 
 JOIN 
-		admissions AS a 
+    admissions AS a 
 ON 
-		p.patient_id = a.patient_id
+    p.patient_id = a.patient_id
 JOIN 
-		doctors AS d
+    doctors AS d
 ON 
-		a.attending_doctor_id = d.doctor_id
+    a.attending_doctor_id = d.doctor_id
 WHERE 
-		a.diagnosis = 'Epilepsy' 
-		AND 
-		d.first_name = 'Lisa';
+    a.diagnosis = 'Epilepsy' 
+    AND 
+    d.first_name = 'Lisa';
