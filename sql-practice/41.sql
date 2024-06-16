fb469a37af18f4f6c9359d1b0aca59ce8f6af336
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:17:35 +0530

    Update 41.sql

--- sql-practice/41.sql
+++ sql-practice/41.sql
@@ -1,14 +1,14 @@
 -- Show patient_id, first_name, last_name from patients whose does not have any records in the admissions table. 
 -- (Their patient_id does not exist in any admissions.patient_id rows.)
 SELECT 
-		p.patient_id, 
-		p.first_name, 
-		p.last_name
+    p.patient_id, 
+    p.first_name, 
+    p.last_name
 FROM 
-		patients AS p 
+    patients AS p 
 LEFT JOIN 
-		admissions AS a 
+    admissions AS a 
 ON 
-		p.patient_id = a.patient_id
+    p.patient_id = a.patient_id
 WHERE 
-		a.patient_id IS NULL;
+    a.patient_id IS NULL;
