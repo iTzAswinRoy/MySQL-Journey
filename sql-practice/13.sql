08bc6abfbe5ba339e8a048bea6136b40ad657731
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:00:24 +0530

    Update 13.sql

--- sql-practice/13.sql
+++ sql-practice/13.sql
@@ -1,8 +1,8 @@
 -- Show the patient id and the total number of admissions for patient_id 579.
 SELECT 
-		patient_id, 
-		COUNT(admission_date)
+    patient_id, 
+    COUNT(admission_date)
 FROM 
-		admissions
+    admissions
 WHERE 
-		patient_id = 579;
+    patient_id = 579;
