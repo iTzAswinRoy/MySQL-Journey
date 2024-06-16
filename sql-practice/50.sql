fe84c7ac774c18d1b189168315f0ebde750683a6
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:24:33 +0530

    Update 50.sql

--- sql-practice/50.sql
+++ sql-practice/50.sql
@@ -1,10 +1,10 @@
 -- For each day display the total amount of admissions on that day. 
 -- Display the amount changed from the previous date.
 SELECT 
-		admission_date, 
-		COUNT(admission_date) AS total_admissions, 
-		(COUNT(*) - LAG(COUNT(admission_date)) OVER (ORDER BY admission_date)) AS date_difference
+    admission_date, 
+    COUNT(admission_date) AS total_admissions, 
+    (COUNT(*) - LAG(COUNT(admission_date)) OVER (ORDER BY admission_date)) AS date_difference
 FROM 
-		admissions
+    admissions
 GROUP BY 
-		admission_date;
+    admission_date;
