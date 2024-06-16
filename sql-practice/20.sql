562c34c1c32b23018fb9d25dbcfa8aae6f1db219
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:04:48 +0530

    Update 20.sql

--- sql-practice/20.sql
+++ sql-practice/20.sql
@@ -1,13 +1,13 @@
 -- Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
 SELECT 
-		a.patient_id, 
-		p.first_name, 
-		p.last_name
+    a.patient_id, 
+    p.first_name, 
+    p.last_name
 FROM 
-		patients AS p
+    patients AS p
 JOIN 
-		admissions AS a
+    admissions AS a
 ON 
-		p.patient_id = a.patient_id
+    p.patient_id = a.patient_id
 WHERE 
-		a.diagnosis = 'Dementia';  
+    a.diagnosis = 'Dementia';  
