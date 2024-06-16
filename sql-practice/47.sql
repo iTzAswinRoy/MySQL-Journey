2d2c957ecb9a908489d06ccd88c08957eb5f7684
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:23:14 +0530

    Update 47.sql

--- sql-practice/47.sql
+++ sql-practice/47.sql
@@ -1,18 +1,18 @@
 -- Show the provinces that has more patients identified as 'M' than 'F'. Must only show full province_name
 SELECT 
-		province_name	
+    province_name	
 FROM 
-		(SELECT 
-					pn.province_name, 
-					SUM(p.gender = 'M') AS male, 
-					SUM(p.gender = 'F') AS female 
-     FROM 
-		     patients AS p
-     JOIN 
-		     province_names AS pn
-     ON 
-		     p.province_id = pn.province_id
-     GROUP BY 
-		     pn.province_name)
+    (SELECT 
+          pn.province_name, 
+          SUM(p.gender = 'M') AS male, 
+          SUM(p.gender = 'F') AS female 
+      FROM 
+          patients AS p
+      JOIN 
+          province_names AS pn
+      ON 
+          p.province_id = pn.province_id
+      GROUP BY 
+          pn.province_name)
 WHERE 
-		male > female;
+    male > female;
