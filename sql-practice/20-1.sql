32c2e074324494175053a8c193207a436b0f018d
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:04:11 +0530

    Update 20-1.sql

--- sql-practice/20-1.sql
+++ sql-practice/20-1.sql
@@ -1,9 +1,9 @@
 -- Display every patient's first_name.
 -- Order the list by the length of each name and then by alphabetically.
 SELECT 
-		first_name
+    first_name
 FROM 
-		patients
+    patients
 ORDER BY 
-		LENGTH(first_name) ASC, 
-		first_name;  
+    LENGTH(first_name) ASC, 
+    first_name;  
