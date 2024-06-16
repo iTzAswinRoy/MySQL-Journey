dfd81bd31ca5e9136243363f799e8e91e19166c5
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:01:27 +0530

    Update 15.sql

--- sql-practice/15.sql
+++ sql-practice/15.sql
@@ -1,9 +1,9 @@
 -- Write a query to find the first_name, last name and birth date of patients who has height greater than 160 and weight greater than 70
 SELECT 
-		first_name, 
-		last_name, 
-		birth_date
+    first_name, 
+    last_name, 
+    birth_date
 FROM 
-		patients
+    patients
 WHERE 
-		height > 160 AND weight > 70;
+    height > 160 AND weight > 70;
