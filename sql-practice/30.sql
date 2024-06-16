bfbc3adbe2003aed82a10d012693b3f49dc66938
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:12:12 +0530

    Update 30.sql

--- sql-practice/30.sql
+++ sql-practice/30.sql
@@ -1,9 +1,9 @@
 -- Show the province_id(s), sum of height; where the total sum of its patient's height is greater than or equal to 7,000.
 SELECT 
-		province_id, SUM(height)
+    province_id, SUM(height)
 FROM 
-		patients
+    patients
 GROUP BY 
-		province_id
+    province_id
 HAVING 
-		SUM(height) >= 7000;
+    SUM(height) >= 7000;
