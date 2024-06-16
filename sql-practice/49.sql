675ebb51b4f5b95fdd92cd3ef3118bf6bf0297d1
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:24:04 +0530

    Update 49.sql

--- sql-practice/49.sql
+++ sql-practice/49.sql
@@ -1,6 +1,6 @@
 -- Show the percent of patients that have 'M' as their gender. 
 -- Round the answer to the nearest hundreth number and in percent form.
 SELECT 
-		CONCAT(ROUND(100 * AVG(gender = 'M'), 2),'%') AS Percentage
+    CONCAT(ROUND(100 * AVG(gender = 'M'), 2),'%') AS Percentage
 FROM 
-		patients;
+    patients;
