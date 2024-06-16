495d5136bfb2237b7448b3991d36484ca79c81cf
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:15:20 +0530

    Update 37.sql

--- sql-practice/37.sql
+++ sql-practice/37.sql
@@ -1,14 +1,14 @@
 -- Display the total amount of patients for each province. Order by descending.
 SELECT 
-		pn.province_name, 
-		COUNT(p.patient_id) As total
+    pn.province_name, 
+    COUNT(p.patient_id) As total
 FROM 
-		patients AS p 
+    patients AS p 
 JOIN 
-		province_names AS pn 
+    province_names AS pn 
 ON 
-		p.province_id = pn.province_id
+    p.province_id = pn.province_id
 GROUP BY 
-		pn.province_id
+    pn.province_id
 ORDER BY 
-		total DESC;
+    total DESC;
