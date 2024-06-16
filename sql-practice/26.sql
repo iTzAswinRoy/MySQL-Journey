ea23476caed82c5f29a61a5e33ba82726c3ea938
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:10:26 +0530

    Update 26.sql

--- sql-practice/26.sql
+++ sql-practice/26.sql
@@ -1,15 +1,15 @@
 -- Show first name, last name and role of every person that is either patient or doctor.
 -- The roles are either "Patient" or "Doctor"
 SELECT 
-		first_name, 
-		last_name, 
-		'Patient' AS role
+    first_name, 
+    last_name, 
+    'Patient' AS role
 FROM 
-		patients
+    patients
 UNION ALL 
 SELECT 
-		first_name, 
-		last_name, 
-		'Doctor' AS role
+    first_name, 
+    last_name, 
+    'Doctor' AS role
 FROM 
-		doctors;
+    doctors;
