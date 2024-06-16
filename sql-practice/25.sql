bd539e0a688ac78cf2a1fd866108fcac57507ce2
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:09:38 +0530

    Update 25.sql

--- sql-practice/25.sql
+++ sql-practice/25.sql
@@ -1,12 +1,12 @@
 -- Show the city and the total number of patients in the city.
 -- Order from most to least patients and then by city name ascending.
 SELECT 
-		city, 
-		COUNT(patient_id) AS total
+    city, 
+    COUNT(patient_id) AS total
 FROM 
-		patients
+    patients
 GROUP BY 
-		city
+    city
 ORDER BY 
-		total DESC, 
-		city ASC;
+    total DESC, 
+    city ASC;
