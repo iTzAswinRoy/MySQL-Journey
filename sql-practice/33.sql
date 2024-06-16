bed8d1b4f1cc7197fa9c7ee31d2e750b667917d9
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:13:33 +0530

    Update 33.sql

--- sql-practice/33.sql
+++ sql-practice/33.sql
@@ -1,9 +1,10 @@
 -- Show all columns for patient_id 542's most recent admission_date.
-SELECT *
+SELECT 
+    *
 FROM 
-		admissions
+    admissions
 GROUP BY 
-		patient_id = 542
+    patient_id = 542
 ORDER BY 
-		admission_date DESC
+    admission_date DESC
 LIMIT 1;
