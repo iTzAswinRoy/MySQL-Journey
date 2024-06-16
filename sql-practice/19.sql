93039cc8a59a4f7047ab16b1ed89e4d019ebea8f
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:03:40 +0530

    Update 19.sql

--- sql-practice/19.sql
+++ sql-practice/19.sql
@@ -1,7 +1,8 @@
 -- Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
-SELECT patient_id, 
-		first_name
+SELECT 
+    patient_id, 
+    first_name
 FROM 
-		patients
+    patients
 WHERE 
-		first_name LIKE 'S____%S';  
+    first_name LIKE 'S____%S';  
