dd7a4256202cf60344e400517f03daffa5ecbf05
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 12:15:44 +0530

    Update 38.sql

--- sql-practice/38.sql
+++ sql-practice/38.sql
@@ -1,14 +1,14 @@
 -- For every admission, display the patient's full name, their admission diagnosis, and their doctor's full name who diagnosed their problem.
 SELECT 
-		CONCAT(p.first_name,' ',p.last_name) AS full_name, 
-		a.diagnosis, CONCAT(d.first_name,' ',D.last_name) AS full_name
+    CONCAT(p.first_name,' ',p.last_name) AS full_name, 
+    a.diagnosis, CONCAT(d.first_name,' ',D.last_name) AS full_name
 FROM 
-		patients AS p 
+    patients AS p 
 JOIN 
-		admissions AS a
+    admissions AS a
 ON 
-		p.patient_id = a.patient_id
+    p.patient_id = a.patient_id
 JOIN 
-		doctors as d 
+    doctors as d 
 ON 
-		d.doctor_id = a.attending_doctor_id;
+    d.doctor_id = a.attending_doctor_id;
