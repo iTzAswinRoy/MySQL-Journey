5ab435d895189ae399ee3d249d157a23aa7e0dc1
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-16 11:59:52 +0530

    Update 12.sql

--- sql-practice/12.sql
+++ sql-practice/12.sql
@@ -1,6 +1,7 @@
 -- Show all the columns from admissions where the patient was admitted and discharged on the same day.
-SELECT *
+SELECT 
+    *
 FROM 
-		admissions
+    admissions
 WHERE 
-		admission_date = discharge_date;
+    admission_date = discharge_date;
