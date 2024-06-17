a33dbadb4bb137d13db82e84a439ccf49b469eac
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-17 22:55:05 +0530

    Create 60.sql

---
+++ sql-practice/60.sql
@@ -0,0 +1,9 @@
+-- Show the company_name, contact_name, fax number of all customers that has a fax number. (not null)
+SELECT 
+		company_name, 
+    contact_name, 
+    fax
+FROM 
+		customers
+WHERE 
+		fax NOT NULL;
