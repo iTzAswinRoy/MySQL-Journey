6d6855b86c91b6d49162a848fa784d20b119f1b3
Aswin Roy C <165917090+iTzAswinRoy@users.noreply.github.com>
2024-06-18 19:28:15 +0530

    Create 4.sql

---
+++ Hacker Rank/4.sql
@@ -0,0 +1,20 @@
+-- P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+-- * 
+-- * * 
+-- * * * 
+-- * * * * 
+-- * * * * *
+SELECT  
+    SELECT '*'
+UNION ALL
+SELECT
+    REPEAT('* ', 2)
+UNION ALL
+SELECT 
+    REPEAT('* ', 3)
+UNION ALL
+SELECT 
+    REPEAT('* ', 4)
+UNION ALL
+SELECT
+    REPEAT('* ', 5);
